31fef591f92d006ff06c28809c84fdbc29884e7c
Frank Bertsch <fbertsch@mozilla.com>
2016-11-18 20:19:51 -0600

    Add explorer and README

---
+++ README.md
@@ -0,0 +1,2 @@
+#MozPing Explorer
+This repository is just a collection of functions that I use to look around pings. I don't imagine many people will have much use for these.
---
+++ explorer.py
@@ -0,0 +1,34 @@
+import re
+
+def json_get(json, json_path):
+    """Get a value based on a json_path key
+    """
+    if not json_path:
+        return json
+    return json_get(json[json_path[0]], json_path[1:])
+
+def walk(json, sep='/', escape='MOZESCAPE'):
+    """Walk a json blob, returning a list of all key paths
+    """
+    explored, unexplored = set(), set(json.keys())
+    
+    while unexplored:
+        next = unexplored.pop()
+        explored.add(next)
+         
+        #don't match on escape chars
+        next_path = re.split(r'(?<!{}){}'.format(escape, sep), next)
+        
+        #remove escape chars before following path
+        val = json_get(json, [np.replace(escape, '') for np in next_path])
+
+        if type(val) is dict:
+            unexplored |= set([sep.join((next, k.replace(sep, escape+sep))) for k in val.keys()])
+    
+    return sorted(list(explored), key=lambda x: x.count(sep))
+
+def find(json, key):
+    """Returns all key paths ending in key
+    """
+    return [k for k in walk(json) if k.endswith(key)]
+    
