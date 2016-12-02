13c7c43f51a3d0eb05073c33665f1d9cf6628232
Frank Bertsch <fbertsch@mozilla.com>
2016-12-02 11:05:55 -0600

    Make find return all paths containing value

--- explorer.py
+++ explorer.py
@@ -14,24 +14,23 @@ def walk(json, sep='/', escape='MOZESCAPE'):
     """Walk a json blob, returning a list of all key paths
     """
     explored, unexplored = set(), set(json.keys())
-    
+
     while unexplored:
         next = unexplored.pop()
         explored.add(next)
-         
+
         #don't match on escape chars
         next_path = re.split(r'(?<!{}){}'.format(escape, sep), next)
-        
+
         #remove escape chars before following path
         val = json_get(json, [np.replace(escape, '') for np in next_path])
 
         if type(val) is dict:
             unexplored |= set([sep.join((next, k.replace(sep, escape+sep))) for k in val.keys()])
-    
+
     return sorted(list(explored), key=lambda x: x.count(sep))
 
-def find(json, key):
-    """Returns all key paths ending in key
+def find(json, needle):
+    """Returns all paths containing needle 
     """
-    return [k for k in walk(json) if k.endswith(key)]
-    
+    return [k for k in walk(json) if needle in k]
