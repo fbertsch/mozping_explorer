827542868e765a1f391b27c79d3d872e6484d8d8
Frank Bertsch <fbertsch@mozilla.com>
2016-12-03 12:53:23 -0600

    Store json paths as tuples

--- mozping_explorer/explorer.py
+++ mozping_explorer/explorer.py
@@ -12,33 +12,28 @@ def json_get(json, json_path):
             return
     return json
 
-def get_escaped_keys(json, sep, escape):
-    return {k.replace(sep, escape+sep) for k in json.keys()}
+def get_escaped_keys(json, sep, escape, prepend=None):
+    if prepend is None:
+        prepend = []
+    return {tuple([p for p in prepend] + [k.replace(sep, escape+sep)]) for k in json.keys()}
 
 def walk(json, sep='/', escape='MOZESCAPE', include_escape=False):
     """Walk a json blob, returning a list of all key paths
     """
-
     #todo: allow sep be multiple chars, escape any special chars in escape
 
-    match_reg = r'(?<!{}){}'.format(escape, '\\' + sep if sep in re_special_chars else sep)
-    explored, unexplored = {}, get_escaped_keys(json, sep, escape)
+    explored, unexplored = set(), get_escaped_keys(json, sep, escape)
 
     while unexplored:
         next = unexplored.pop()
         explored.add(next)
+        next_val = json_get(json, [n.replace(escape, '') for n in next])
 
-        #don't match on escape chars
-        next_path = re.split(match_reg, next)
-
-        #remove escape chars before following path
-        val = json_get(json, [np.replace(escape, '') for np in next_path])
-
-        if type(val) is dict:
-            unexplored |= {sep.join((next, k)) for k in get_escaped_keys(val, sep, escape)}
+        if type(next_val) is dict:
+            unexplored |= get_escaped_keys(next_val, sep, escape, prepend=next)
 
     map_func = lambda x: x if include_escape else x.replace(escape, '')
-    return {map_func(x) for x in explored}
+    return {map_func(sep.join(x)) for x in explored}
 
 def find(json, needle):
     """Returns all paths containing needle 
