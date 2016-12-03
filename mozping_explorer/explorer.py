7814986945999d27098509e39bb5e76a9898747b
Frank Bertsch <fbertsch@mozilla.com>
2016-12-03 12:28:46 -0600

    Make everything unordered
    json is inherently unordered.

--- mozping_explorer/explorer.py
+++ mozping_explorer/explorer.py
@@ -1,5 +1,7 @@
 import re
 
+re_special_chars = {'.', '\\', '$', '*', '^'}
+
 def json_get(json, json_path):
     """Get a value based on a json_path key
     """
@@ -11,13 +13,16 @@ def json_get(json, json_path):
     return json
 
 def get_escaped_keys(json, sep, escape):
-    return [k.replace(sep, escape+sep) for k in json.keys()]
+    return {k.replace(sep, escape+sep) for k in json.keys()}
 
 def walk(json, sep='/', escape='MOZESCAPE', include_escape=False):
     """Walk a json blob, returning a list of all key paths
     """
-    match_reg = r'(?<!{}){}'.format(escape, sep)
-    explored, unexplored = set(), set(get_escaped_keys(json, sep, escape))
+
+    #todo: allow sep be multiple chars, escape any special chars in escape
+
+    match_reg = r'(?<!{}){}'.format(escape, '\\' + sep if sep in re_special_chars else sep)
+    explored, unexplored = {}, get_escaped_keys(json, sep, escape)
 
     while unexplored:
         next = unexplored.pop()
@@ -30,12 +35,12 @@ def walk(json, sep='/', escape='MOZESCAPE', include_escape=False):
         val = json_get(json, [np.replace(escape, '') for np in next_path])
 
         if type(val) is dict:
-            unexplored |= set([sep.join((next, k)) for k in get_escaped_keys(val, sep, escape)])
+            unexplored |= {sep.join((next, k)) for k in get_escaped_keys(val, sep, escape)}
 
     map_func = lambda x: x if include_escape else x.replace(escape, '')
-    return set([map_func(x) for x])
+    return {map_func(x) for x in explored}
 
 def find(json, needle):
     """Returns all paths containing needle 
     """
-    return [k for k in walk(json) if needle in k]
+    return {k for k in walk(json) if needle in k}
