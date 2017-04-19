70e54adc75d4dba089edb1f6fd211c8b5dd61095
Frank Bertsch <fbertsch@mozilla.com>
2017-04-19 14:54:20 -0500

    Use autodiscovery of packages

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='mozping_explorer',
       author_email='fbertsch@mozilla.com',
       description='Exploration functions for mozilla telemetry pings',
       url='https://github.com/fbertsch/mozping_explorer',
-      packages=['mozping_explorer'],
+      packages=find_packages(),
       tests_require=['pytest'])
